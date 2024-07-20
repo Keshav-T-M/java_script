69503317b0dab8b9215886c62fdca347d01101ad
Keshavshetty707 <121680990+Keshavshetty707@users.noreply.github.com>
2024-07-20 08:17:11 +0000

    operactions

---
+++ Basics/operactions/example.js
@@ -0,0 +1,37 @@
+//basic operactions
+console.log("addition",15+10)
+console.log("subtraction",15-10)
+console.log("multiplication",15*10)
+console.log("exponentiation",15**10)
+console.log("division",15/10)
+console.log("modulus",15%10)
+
+//some intresting operactions
+let value = 3
+let submit = -value
+console.log("\nsubmit = -value \n submit = ",submit)//-3
+
+console.log("\n",true)
+console.log("+true =",+true)
+console.log("\n",+"")
+
+let numb1 , numb2 , numb3 
+num1 = numb2 = numb3 = 2 + 2
+
+console.log("numb1=",numb1 , "numb2=", numb2 , "numb3=", numb3)
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+
+let str1 = "h1"
+let str2 = " keshav"
+let str3 = str1 + str2 
+console.log(str3)
+
+console.log("\nadding string(1) and number(2) =","1"+ 2)
+console.log("\nadding numbe(1) and string(2) =",1 + "2")
+console.log("\nadding string(1) + number(2) + number(2) =","1" + 2 + 2)
+console.log("\nadding number(1) + number(2) + string(2) =",1 + 2 + "2")
+console.log((3 + 4) - 7 * 8 % 10)
+
