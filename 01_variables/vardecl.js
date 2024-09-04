fc83f25e2575f4b816bf208a1310cdcb9bc34bb1
Keshavshetty707 <121680990+Keshavshetty707@users.noreply.github.com>
2024-09-04 05:30:54 +0000

    "Deleted file 01_variables/varDecl.js"

---
+++ 01_variables/vardecl.js
@@ -0,0 +1,9 @@
+var my_firstName = "John"; //valid
+var _myLastName$ = "Doe"; //valid
+// var 123myAge = 25; //not valid
+var $cityName = "New York"; //valid
+// var my@Email = "keshav123@gmail..com"; //not valid
+
+console.log(my_firstName)
+console.log(_myLastName$)
+console.log($cityName)
