f6e8d0a9bf03690ca9efed7258306ccdd1e8abfc
Keshavshetty707 <121680990+Keshavshetty707@users.noreply.github.com>
2024-09-05 07:25:01 +0000

    primitive datatypes of js incomplete

---
+++ 02_datatypes/01_primitive_DT.js
@@ -0,0 +1,22 @@
+// String : Represents a sequence of characters enclosed in single or double quotes
+var myName = "Keshav" // var myName = keshav (keshav is not defined , in this code)
+console.log(myName)
+
+//Number:Represents numeric values , including integers and floating-point numbers
+var myNumber = 7;//1 ,-1 , 1.0 etc
+console.log(mynumber)
+
+//Boolean:Represents a logical entity with two values: true or false.
+var isRaining = false
+var isFloorWet = true
+console.log(isRaining)
+console.log(isFloorWet)
+
+//Undefined
+
+
+//Null:Represents the absence of a value. It is often used to explicitly indicate that a variable or object property has no assigned value.
+//Bigint
+//Symbol
+
+
