6efa06c84e3ac68248b096fe12ae01511e56c0e7
Keshavshetty707 <mkeshav9241217275@gmail.com>
2024-12-11 11:33:53 +0530

    parseFloat operations

--- 02_datatypes/03_extra.js
+++ 02_datatypes/03_extra.js
@@ -1,18 +1,23 @@
 //parseInt
 const myString = "42";
 const myString1 = "32.5"
-const myString3 = "055"
-const myString4 = "123"
-const myString5 = "     345"
+const myString2 = "055"
+const myString3 = "123"
+const myString4 = "     345"
 const myNumber = parseInt(myString);//default base-10
 const myNumber1 = parseInt(myString1);//decimal part is truncated
-const myNumber3 = parseInt(myString3);// leading zeros are ignored
-const myNumber4 = parseInt(myString4, 10);// explicitly specify base-10
-const myNumber5 = parseInt(myString5);// whiteSpace is ignored
+const myNumber2 = parseInt(myString2);// leading zeros are ignored
+const myNumber3 = parseInt(myString3, 10);// explicitly specify base-10
+const myNumber4 = parseInt(myString4);// whiteSpace is ignored
 console.log("myString:",myNumber)
 console.log("myString1:",myNumber1)
 console.log("myString3:",myNumber3)
 console.log("myString4:",myNumber4)
 console.log("myString5:",myNumber5)
 
+
+//parseFloat
+const mystr = '3.14';
+const myNumb1 = parseFloat(mystr);
+console.log("mystr:",myNumb1)
 
